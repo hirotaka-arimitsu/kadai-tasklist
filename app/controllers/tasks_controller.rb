64ccdf3788945b608a6c055661ad736987a48a57
hirotaka.arimitsu <arimitu1030@yahoo.co.jp>
2020-07-20 14:02:15 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,10 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   
   def index
-    @tasks = Task.all
+    if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end    
   end
 
   def show
@@ -10,11 +13,11 @@ class TasksController < ApplicationController
   end
 
   def new
-    @task = Task.new
+    @task = current_user.tasks.build  # form_with 用
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save 
       flash[:success] = 'Task が正常に作成されました'
@@ -44,16 +47,23 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     
-    flash[:success]='Task は正常に削除されました'
+    flash[:success]='タスクは正常に削除されました'
     redirect_to tasks_url
   end
 
   private
   
   def set_task
-    @task = Task.find(params[:id])
+    @task = Task.find_by(id: params[:id])
+    #@task = Task.find(params[:id])
   end
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ApplicationRecord
   
   has_secure_password  
   
+  has_many :tasks
   
 end
